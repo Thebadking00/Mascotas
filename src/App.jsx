0a565c8e2959e9a032983562dafddb4bf7202bcb
Thebadking00 <al222311228@gmail.com>
2025-02-23 18:54:11 -0600

    se le agrega https://3.129.250.115/mascotas/

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [filteredData, setFilteredData] = useState([]);
 
   useEffect(() => {
-    fetch("http://3.129.250.115/mascotas/")
+    fetch("https://3.129.250.115/mascotas/")
       .then((response) => response.json())
       .then((data) => {
         setData(data);
